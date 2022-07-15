86919bc83752118034b2df0a4f0ac33ecc15668a
koushik <koushik.dey@live.com>
2022-07-16 00:30:41 +0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.png';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [userName, setUsername] = useState('');
+  const [followers, setFollowers] = useState('');
+  const [following, setFollowing] = useState('');
+  const [repos, setRepos] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [error, setError] = useState(null);
+
+
   return (
     <div className="App">
       <header className="App-header">
