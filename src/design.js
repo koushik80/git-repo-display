1a7377217cd7ff350d57ff17dab5fac9d5a0fa69
koushik <koushik.dey@live.com>
2022-07-18 01:16:57 +0300

    design-2

--- src/design.js
+++ src/design.js
@@ -1,7 +1,7 @@
 // Some random colors
 const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 
-const numBalls = 50;
+const numBalls = 100;
 const balls = [];
 
 for (let i = 0; i < numBalls; i++) {
@@ -32,7 +32,7 @@ balls.forEach((el, i, ra) => {
       { transform: `translate(${to.x}rem, ${to.y}rem)` }
     ],
     {
-      duration: (Math.random() + 1) * 2000, // random duration
+      duration: (Math.random() + 1) * 3000, // random duration
       direction: "alternate",
       fill: "both",
       iterations: Infinity,
